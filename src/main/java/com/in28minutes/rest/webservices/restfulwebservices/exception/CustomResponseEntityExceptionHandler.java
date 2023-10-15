20b5e26b164f07caf8aaf46dbc6340900b283fea
Edgar Benitez <edgarbenitez92@gmail.com>
2023-10-15 20:16:12 -0300

    Incorporación de validaciones en el restful api
    -. Nueva dependencia de spring-boot-starter-validation
    -. Función de mapping debe tener una anotación de @Valid para poder consumir la nueva dependencia
    -. Cada variabe de la clase User se le puede añadir diferentes validaciones segun aplique
    -. Adicional a ello, puede añadirse un message donde especifíca de mejor manera el error dado
    -. Manejamos esos errores en una nueva función en nuestro CustomResponseEntityExceptionHandler y lo respondemos como un bad_request

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/exception/CustomResponseEntityExceptionHandler.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/exception/CustomResponseEntityExceptionHandler.java
@@ -3,8 +3,11 @@ package com.in28minutes.rest.webservices.restfulwebservices.exception;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -19,15 +22,27 @@ public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptio
 	public final ResponseEntity<ErrorDetails> handleServerException(Exception ex, WebRequest request) throws Exception {
 		LocalDateTime date = LocalDateTime.now();
 		ErrorDetails errorDetails = new ErrorDetails(date, ex.getMessage(), request.getDescription(false));
-		ResponseEntity<ErrorDetails> internalServerError = new ResponseEntity(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+		ResponseEntity<ErrorDetails> internalServerError = new ResponseEntity(errorDetails,
+				HttpStatus.INTERNAL_SERVER_ERROR);
 		return internalServerError;
 	}
 
 	@ExceptionHandler(UserNotFoundException.class)
-	public final ResponseEntity<ErrorDetails> handleUserNotFoundException(Exception ex, WebRequest request) throws Exception {
+	public final ResponseEntity<ErrorDetails> handleUserNotFoundException(Exception ex, WebRequest request)
+			throws Exception {
 		LocalDateTime date = LocalDateTime.now();
 		ErrorDetails errorDetails = new ErrorDetails(date, ex.getMessage(), request.getDescription(false));
-		ResponseEntity<ErrorDetails> internalServerError = new ResponseEntity(errorDetails, HttpStatus.NOT_FOUND);
-		return internalServerError;
+		ResponseEntity<ErrorDetails> notFoundError = new ResponseEntity(errorDetails, HttpStatus.NOT_FOUND);
+		return notFoundError;
+	}
+
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+		HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+
+		LocalDateTime date = LocalDateTime.now();
+		ErrorDetails errorDetails = new ErrorDetails(date, ex.getFieldError().getDefaultMessage(), request.getDescription(false));
+		ResponseEntity badRequestError = new ResponseEntity(errorDetails, HttpStatus.BAD_REQUEST);
+		return badRequestError;
 	}
 }
