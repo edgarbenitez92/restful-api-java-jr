037696f2ec54305639f550578072b09ecae847a5
Edgar Benitez <edgarbenitez92@gmail.com>
2023-10-16 16:32:57 -0300

    Uso de librería i18n para manejo de respuestas por idioma locale
    -. Se crea messages.properties con los diferentes mensajes a mostrar
    -. Se debe usar el Accept-Language en los headers del request
    -. En el controller se hace un return del message esperado

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloword/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloword/HelloWorldController.java
@@ -1,5 +1,9 @@
 package com.in28minutes.rest.webservices.restfulwebservices.helloword;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,6 +12,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 public class HelloWorldController {
+
+	private MessageSource messageSource;
+
+	public HelloWorldController(MessageSource messageSource) {
+		this.messageSource = messageSource;
+	}
 
 	@GetMapping(path = "/hello-word")
 	public String helloWordApi() {
@@ -18,10 +28,16 @@ public class HelloWorldController {
 	public HelloWordBean helloWordBeanApi() {
 		return new HelloWordBean("hello word bean");
 	}
-	
+
 	@GetMapping(path = "/hello-word/path-variable/{name}")
 	public HelloWordBean helloWordPathVariableApi(@PathVariable String name) {
-		return new HelloWordBean(String.format("Hello word, %s",  name));
+		return new HelloWordBean(String.format("Hello word, %s", name));
 		// return new HelloWordBean("hello word" + name);
+	}
+
+	@GetMapping(path = "/hello-word-i18n")
+	public String helloWordApi18n() {
+		Locale userLocale = LocaleContextHolder.getLocale();
+		return messageSource.getMessage("good.morning.message", null, "Default Message", userLocale);
 	}
 }
