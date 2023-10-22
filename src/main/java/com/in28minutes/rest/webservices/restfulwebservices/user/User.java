4c4e1ac76a50fdd1e945b60df6ea83c4a8ae279e
Edgar Benitez <edgarbenitez92@gmail.com>
2023-10-21 22:34:23 -0300

    Se trabaja el ignorar propiedades en el JSON de la restfulApi
    -. De manera estática se usa el JSONIgnore y JSONPropertiesIgnore
    -. De manera dinámica podemos crear un MappingJacksonValue e ir asignando filtros para ver los datos que deseemos mostrar por endpoint
    -. La forma estática aplica para todo el controller
    -. La formá dinámica aplica para método en específico
    -. Se deja tarea para refactorizar código duplicado

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
@@ -2,14 +2,19 @@ package com.in28minutes.rest.webservices.restfulwebservices.user;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 public class User {
 	private Integer id;
-	@Size(min=2, message="Name should have at least 2 characters")
+
+	@Size(min = 2, message = "Name should have at least 2 characters")
+	@JsonProperty("user_name")
 	private String name;
-	@Past(message="Birthdate should be in the past")
+
+	@Past(message = "Birthdate should be in the past")
 	private LocalDate birthDate;
 
 	public User(Integer id, String name, LocalDate birthDate) {
